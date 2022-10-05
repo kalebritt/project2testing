6e3d77e4e590e78b57967fa92cee206e2b171436
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-04 18:42:02 -0600

    got dayjs to function: need to write function to display

---
+++ script.js
@@ -0,0 +1,14 @@
+// const dayjs = require("dayjs");
+let now = dayjs();
+console.log('hello');
+console.log(now.format("dddd, MMMM D YYYY"));
+
+// let d = new Date(2022, 8, 18);
+// let day = dayjs(d);
+// console.log(day);
+
+// dayjs()
+//   .startOf("month")
+//   .add(1, "day")
+//   .set("year", 2018)
+//   .format("YYYY-MM-DD HH:mm:ss");
